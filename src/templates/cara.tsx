7e2be72f102066cb070cea44461e2acab5adf2b6
Agus Imam Fauzi <agus7fauzi@gmail.com>
2022-06-19 01:29:34 +0700

    add auto dark or light theme based on time

--- src/templates/cara.tsx
+++ src/templates/cara.tsx
@@ -1,20 +1,35 @@
-import * as React from "react"
+import React, { useEffect } from "react"
 import { Parallax } from "@react-spring/parallax"
+import { useColorMode } from "theme-ui"
 import Layout from "../components/layout"
 import Hero from "../components/hero"
 import Projects from "../components/projects"
 import About from "../components/about"
 import Contact from "../components/contact"
 
-const Cara = () => (
-  <Layout>
-    <Parallax pages={5}>
-      <Hero offset={0} factor={1} />
-      <Projects offset={1} factor={2} />
-      <About offset={3} factor={1} />
-      <Contact offset={4} factor={1} />
-    </Parallax>
-  </Layout>
-)
+const Cara = () => {
+  const [colorMode, setColorMode] = useColorMode()
+
+  useEffect(() => {
+    const hours = new Date().getHours()
+    const isDayTime = hours > 6 && hours < 20
+    
+    if (isDayTime && colorMode === 'dark')
+      setColorMode('light')
+    else
+      setColorMode('dark')
+  }, [])
+
+  return (
+    <Layout>
+      <Parallax pages={5}>
+        <Hero offset={0} factor={1} />
+        <Projects offset={1} factor={2} />
+        <About offset={3} factor={1} />
+        <Contact offset={4} factor={1} />
+      </Parallax>
+    </Layout>
+  )
+}
 
 export default Cara
